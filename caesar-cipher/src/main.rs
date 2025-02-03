afd4c2753d247e0cb7e3ec7c9bf8f507e5214e6a
Sami Zakir Ahmed <54m1z4k@gmail.com>
2025-02-03 23:50:14 +0000

    Fixed character wrapping bug

--- caesar-cipher/src/main.rs
+++ caesar-cipher/src/main.rs
@@ -1,44 +1,31 @@
 fn main() {
     println!("Hello, world!");
 
-    let string_to_encrypt = "hello world".to_uppercase();
+    let string_to_encrypt = "hello world!".to_uppercase();
     let string_vec: Vec<char> = string_to_encrypt.chars().collect();
     let shift: usize = 5;
 
     let possible_chars: Vec<char> = ('A'..='Z').into_iter().collect::<Vec<char>>();
     let mut encrypted: Vec<char> = Vec::new();
 
-    // let x = string_vec.iter().map(|c| {
-    //     if possible_chars.contains(&c) {
-    //         let shifted_char = (&c as u8) + shift as u8;
-    //         return shifted_char as char;
-    //     } else {
-    //         return &c as char;
-    //     }
-    // });
-
-    for c in string_vec {
+    for c in &string_vec {
         if possible_chars.contains(&c) {
-            let mut shifted_char = (c as u8) + shift as u8;
-            if shifted_char > ('Z' as u8) {
-                shifted_char -= ('A' as u8) + ('Z' as u8) - shifted_char;
-            }
-            encrypted.push(shifted_char as char);
+            let index = (*c as u8 - b'A') as usize;
+            let shifted_index = (index + shift) % 26;
+            encrypted.push((shifted_index as u8 + b'A') as char);
         } else {
-            encrypted.push(c as char);
+            encrypted.push(*c);
         }
     }
 
     let mut decrypted: Vec<char> = Vec::new();
-    for c in encrypted.clone() {
+    for c in &encrypted.clone() {
         if possible_chars.contains(&c) {
-            let mut shifted_char = (c as u8) - shift as u8;
-            if shifted_char < ('A' as u8) {
-                shifted_char += ('Z' as u8) - ('A' as u8) - shifted_char;
-            }
-            decrypted.push(shifted_char as char);
+            let index = (*c as u8 - b'A') as usize;
+            let shifted_index = (index + 26 - shift) % 26;
+            decrypted.push((shifted_index as u8 + b'A') as char);
         } else {
-            decrypted.push(c as char);
+            decrypted.push(*c);
         }
     }
 
