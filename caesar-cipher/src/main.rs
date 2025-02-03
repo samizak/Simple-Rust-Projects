022973a389e6f5292a859b6df2caf46658425824
Sami Zakir Ahmed <54m1z4k@gmail.com>
2025-02-03 23:57:08 +0000

    Minor refactoring

--- caesar-cipher/src/main.rs
+++ caesar-cipher/src/main.rs
@@ -1,39 +1,40 @@
 fn main() {
-    println!("Hello, world!");
-
-    let string_to_encrypt = "hello world!".to_uppercase();
-    let string_vec: Vec<char> = string_to_encrypt.chars().collect();
     let shift: usize = 5;
+    let string_to_encrypt = "hello world!";
+    let string_vec: Vec<char> = string_to_encrypt.to_uppercase().chars().collect();
 
     let possible_chars: Vec<char> = ('A'..='Z').into_iter().collect::<Vec<char>>();
-    let mut encrypted: Vec<char> = Vec::new();
 
-    for c in &string_vec {
-        if possible_chars.contains(&c) {
-            let index = (*c as u8 - b'A') as usize;
-            let shifted_index = (index + shift) % 26;
-            encrypted.push((shifted_index as u8 + b'A') as char);
-        } else {
-            encrypted.push(*c);
-        }
-    }
+    let encrypted: Vec<char> = string_vec
+        .iter()
+        .map(|c| {
+            if possible_chars.contains(c) {
+                let index = (*c as u8 - b'A') as usize;
+                let shifted_index = (index + shift) % 26;
+                return (shifted_index as u8 + b'A') as char;
+            } else {
+                return *c;
+            }
+        })
+        .collect();
 
-    let mut decrypted: Vec<char> = Vec::new();
-    for c in &encrypted.clone() {
-        if possible_chars.contains(&c) {
-            let index = (*c as u8 - b'A') as usize;
-            let shifted_index = (index + 26 - shift) % 26;
-            decrypted.push((shifted_index as u8 + b'A') as char);
-        } else {
-            decrypted.push(*c);
-        }
-    }
+    let decrypted: Vec<char> = encrypted
+        .iter()
+        .map(|c| {
+            if possible_chars.contains(c) {
+                let index = (*c as u8 - b'A') as usize;
+                let shifted_index = (index + 26 - shift) % 26;
+                return (shifted_index as u8 + b'A') as char;
+            } else {
+                return *c;
+            }
+        })
+        .collect();
 
-    let test: String = encrypted.iter().collect();
-    let test2: String = decrypted.iter().collect();
+    let encrypted_str: String = encrypted.iter().collect();
+    let decrypted_str: String = decrypted.iter().collect();
 
     println!("Before: {}", string_to_encrypt);
-    println!("After: {}", test);
-
-    println!("descrypted: {}", test2);
+    println!("Encrypted: {}", encrypted_str);
+    println!("Decrypted: {}", decrypted_str);
 }
